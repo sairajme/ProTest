8175d4c882798988f9352338598f015d2933833f
sairaj n <sairaj.n@securew2.com>
2019-09-01 11:48:20 +0530

    prog detials

---
+++ src/test/java/TestProject/Test1/testMain.java
@@ -0,0 +1,12 @@
+package TestProject.Test1;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is from Test - ");
+
+	}
+
+}
